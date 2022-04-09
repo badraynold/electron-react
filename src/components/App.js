4abe60fcb7e25320cd6dac5610073a34d13525c6
Rafał Łukawski <rs.lukawski@gmail.com>
2022-04-09 16:32:24 +0200

    add counter

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,16 @@
-import React from 'react'
+import React, { useState } from "react";
 
 const App = () => {
-	return (
-		<div className='app'>
-			<h1>React Electron Boilerplate</h1>
-			<p>This is a simple boilerplate for using React with Electron</p>
-		</div>
-	)
-}
+  const [counter, setCounter] = useState(0);
+  return (
+    <div className="app">
+      <h1>React Electron Boilerplate</h1>
+      <p>This is a simple boilerplate for using React with Electron</p>
+      <button onClick={() => setCounter((data) => data + 1)}>
+        Counter: {counter}
+      </button>
+    </div>
+  );
+};
 
-export default App
+export default App;
