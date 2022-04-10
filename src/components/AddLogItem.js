9ee3bddff0e2a5d0947d205785287b19d3fbdd32
Rafał Łukawski <rs.lukawski@gmail.com>
2022-04-10 04:25:30 +0200

    add AddLogItem

---
+++ src/components/AddLogItem.js
@@ -0,0 +1,56 @@
+import React, { useState } from "react";
+import { Card, Form, Row, Col, Button } from "react-bootstrap";
+
+export const AddLogItem = () => {
+  const [text, setText] = useState("");
+  const [user, setUser] = useState("");
+  const [priority, setPriority] = useState();
+
+  return (
+    <Card className="mt-5 mb-3">
+      <Card.Body>
+        <Form>
+          <Row className="my-3">
+            <Col>
+              <Form.Control
+                placeholder="Log"
+                value={text}
+                onChange={(e) => setText(e.target.value)}
+              ></Form.Control>
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+              <Form.Control
+                placeholder="User"
+                value={user}
+                onChange={(e) => setUser(e.target.value)}
+              ></Form.Control>
+            </Col>
+            <Col>
+              <Form.Control
+                as="select"
+                value={priority}
+                onChange={(e) => setPriority(e.target.value)}
+              >
+                <option value="0">Select Priority</option>
+                <option value="low">Low</option>
+                <option value="moderate">Moderate</option>
+                <option value="high">High</option>
+              </Form.Control>
+            </Col>
+          </Row>
+          <Row className="my-3">
+            <Col className="d-grid">
+              <Button type="submit" variant="secondary">
+                Add Log
+              </Button>
+            </Col>
+          </Row>
+        </Form>
+      </Card.Body>
+    </Card>
+  );
+};
+
+export default AddLogItem;
--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import Container from "react-bootstrap/Container";
 import Table from "react-bootstrap/Table";
 import LogItem from "./LogItem";
+import AddLogItem from "./AddLogItem";
 
 const App = () => {
   const [counter, setCounter] = useState(0);
@@ -31,6 +32,7 @@ const App = () => {
 
   return (
     <Container>
+      <AddLogItem />
       <Table>
         <thead>
           <tr>
